8e65cbc4fbe4f9ac7610d6c9a1a3c5de05effe5d
Tyrfly <1071131161@qq.com>
2025-12-30 15:18:20 +0800

    feat: 实现分片文件合并功能，优化上传处理逻辑

--- api/mpfile.go
+++ api/mpfile.go
@@ -1,19 +1,30 @@
 package api
 
 import (
+	"errors"
 	"filestore-server/pkg/dao"
 	"filestore-server/pkg/mw"
 	redispool "filestore-server/pkg/redis"
 	"filestore-server/service"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gomodule/redigo/redis"
 )
+
+func getChunkFilePath(uploadID string, chunkIndex int) string {
+	return "/data/" + uploadID + "/" + strconv.Itoa(chunkIndex)
+}
+
+func getFilePath(uploadID string) string {
+	return "/data/" + uploadID
+}
 
 func UploadInitMutipart(c *gin.Context) {
 	infoVal, ok := c.Get(mw.CtxUploadInitKey)
@@ -51,7 +62,8 @@ func UploadPartHandler(c *gin.Context) {
 	uploadID := c.GetString(mw.CtxUploadIDKey)
 	chunkIndex := c.GetInt(mw.CtxChunkIndexKey)
 
-	fpath := "/data/" + uploadID + "/" + strconv.Itoa(chunkIndex)
+	fpath := getChunkFilePath(uploadID, chunkIndex)
+
 	fd, err := os.Create(fpath)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Create file path error"})
@@ -125,11 +137,26 @@ func CompleteUploadHandler(c *gin.Context) {
 		return
 	}
 
-	location := "/data/" + info.UploadID
+	location := getFilePath(info.UploadID)
+
+	destFile, err := os.Create(location)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create file"})
+		return
+	}
+
+	err = parallelMergeMutiPartFile(destFile, info.UploadID, info.ChunkCount, int64(info.ChunkSize))
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to merge mutipart file"})
+		return
+	}
 	if err := dao.SaveFileMeta(c.Request.Context(), info.FileHash, filename, info.FileSize, location); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to save file meta"})
 		return
 	}
+
 	if err := service.SaveUserFileMeta(c.Request.Context(), username, info.FileHash, info.FileSize, filename); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to save user file meta"})
 		return
@@ -142,4 +169,54 @@ func CompleteUploadHandler(c *gin.Context) {
 			"filename": filename,
 		},
 	})
+}
+
+func parallelMergeMutiPartFile(destFile *os.File, uploadID string, totalCount int, chunkSize int64) error {
+
+	limit := make(chan struct{}, 5)
+	errChan := make(chan error, totalCount)
+
+	var wg sync.WaitGroup
+
+	for i := 1; i <= totalCount; i++ {
+		wg.Add(1)
+		limit <- struct{}{}
+		go func(idx int) {
+			defer wg.Done()
+			defer func() { <-limit }()
+
+			fpath := getChunkFilePath(uploadID, idx)
+
+			f, err := os.Open(fpath)
+			if err != nil {
+				errChan <- err
+				return
+			}
+
+			defer f.Close()
+			offset := int64(idx-1) * chunkSize
+			offWriter := io.NewOffsetWriter(destFile, offset)
+			_, err = io.Copy(offWriter, f)
+			if err != nil {
+				errChan <- err
+				return
+			}
+
+		}(i)
+
+	}
+
+	wg.Wait()
+	close(errChan)
+
+	var errList error
+	for e := range errChan {
+		errList = errors.Join(errList, e)
+	}
+
+	if errList != nil {
+		log.Printf("合并过程中发生多处错误: %v", errList)
+		return errList // 此时返回的是包含所有错误信息的对象
+	}
+	return nil
 }
